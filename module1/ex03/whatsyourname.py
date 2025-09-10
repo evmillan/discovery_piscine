dcd98e326cca4edc272c974796f7b812d977e7f3
Emmanuel Millan Contreras <evmillan@netscape.net>
2025-09-10 15:16:46 +0200

    Module1.Ex03. First Upload

---
+++ module1/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+first_name = input("Hey, what's your first name? : ")
+last_name = input("And your last name? : ")
+whole_name = first_name + " " + last_name
+
+print("Well, pleased to meet you,", whole_name + ".")
+print(f"Well, pleased to meet you, {first_name} {last_name}.")
